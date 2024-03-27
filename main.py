3cd8b90c3f4495d965c65831be2c7c888d061a3b
강상원 <kevink1113@u.sogang.ac.kr>
2024-03-27 14:16:59 +0900

    Update on Local Master Branch

--- main.py
+++ main.py
@@ -23,4 +23,6 @@ if __name__ == '__main__':
 
     print_hi("Hello World Updates on GitHub")
 
+    print_hi("Hello World Updates on Local Master Branch")
+
 # https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
